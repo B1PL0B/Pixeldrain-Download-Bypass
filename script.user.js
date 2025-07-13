d37fa76a22dc9e7c01ed890fc6d1ad392c8019d8
Hasanur Rahman Biplob <167545281+B1PL0B@users.noreply.github.com>
2025-07-13 18:24:12 +0600

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -7,27 +7,29 @@
 // @match        https://pixeldrain.com/u/*
 // @grant        none
 // @run-at       document-start
+// @updateURL    https://update.greasyfork.org/scripts/542461/Pixeldrain%20Download%20Bypass.user.js
+// @downloadURL  https://update.greasyfork.org/scripts/542461/Pixeldrain%20Download%20Bypass.user.js
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
- 
+
     // --- Configuration ---
     // The new domain you want to redirect to.
     const newDomain = 'https://pixel.asianrev.fun';
     // -------------------
- 
+
     // Get the path from the URL (e.g., "/u/bXCUGPMA")
     const path = window.location.pathname;
- 
+
     // Extract the file ID by splitting the path and taking the last part.
     const fileId = path.split('/').pop();
- 
+
     // Ensure we actually got a file ID before redirecting.
     if (fileId) {
         // Construct the new URL.
         const redirectUrl = `${newDomain}/${fileId}`;
- 
+
         // Perform the redirect immediately.
         window.location.href = redirectUrl;
     }
