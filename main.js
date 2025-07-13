ae37162276c2d46e197098071d18276e48b8a088
Hasanur Rahman Biplob <167545281+B1PL0B@users.noreply.github.com>
2025-07-13 18:16:29 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Pixeldrain Download Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.1
+// @description  Download pixeldrain file without any limitation.
+// @author       BIPLOB
+// @match        https://pixeldrain.com/u/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+ 
+    // --- Configuration ---
+    // The new domain you want to redirect to.
+    const newDomain = 'https://pixel.asianrev.fun';
+    // -------------------
+ 
+    // Get the path from the URL (e.g., "/u/bXCUGPMA")
+    const path = window.location.pathname;
+ 
+    // Extract the file ID by splitting the path and taking the last part.
+    const fileId = path.split('/').pop();
+ 
+    // Ensure we actually got a file ID before redirecting.
+    if (fileId) {
+        // Construct the new URL.
+        const redirectUrl = `${newDomain}/${fileId}`;
+ 
+        // Perform the redirect immediately.
+        window.location.href = redirectUrl;
+    }
+})();
